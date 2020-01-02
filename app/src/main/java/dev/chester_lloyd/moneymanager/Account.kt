ee736d56f7e5ed870e0e811eaa75670a4c80113d
Chester Lloyd <33861948+ChesterLloyd@users.noreply.github.com>
2020-01-02 21:19:30 +0000

    Added list view for accounts page

---
+++ app/src/main/java/dev/chester_lloyd/moneymanager/Account.kt
@@ -0,0 +1,22 @@
+package dev.chester_lloyd.moneymanager
+
+class Account {
+    var accountID:Int?=null
+    var name:String?=null
+    var balance:Double?=null
+    var icon:Int?=R.drawable.ic_local_atm_white_24dp
+    var circle:Int?=R.drawable.ic_circle_green
+
+    constructor(accountID:Int, name:String, balance:Double, icon:Int?, circle:Int?) {
+        this.accountID = accountID
+        this.name = name
+        this.balance = balance
+//      Preset to green circle and cash icon if these are null
+        if (icon != null) {
+            this.icon = icon
+        }
+        if (circle != null) {
+            this.circle = circle
+        }
+    }
+}
--- app/src/main/java/dev/chester_lloyd/moneymanager/ui/accounts/AccountsFragment.kt
+++ app/src/main/java/dev/chester_lloyd/moneymanager/ui/accounts/AccountsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.BaseAdapter
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import dev.chester_lloyd.moneymanager.Account
 import dev.chester_lloyd.moneymanager.R
+import kotlinx.android.synthetic.main.account.view.*
+import kotlinx.android.synthetic.main.fragment_accounts.view.*
 
 class AccountsFragment : Fragment() {
 
@@ -22,10 +24,44 @@ class AccountsFragment : Fragment() {
         accountsViewModel =
             ViewModelProviders.of(this).get(AccountsViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_accounts, container, false)
-        val textView: TextView = root.findViewById(R.id.text_gallery)
-        accountsViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+
+        var listAccounts = ArrayList<Account>()
+        listAccounts.add(Account(1,"Cash",30.23,null,null))
+        listAccounts.add(Account(2,"PayPal",56.00, R.drawable.ic_paypal_brands, R.drawable.ic_circle_paypal))
+        val myAccountsAdapter = MyAccountsAdapter(listAccounts)
+        root.lvAccounts.adapter = myAccountsAdapter
+
         return root
+    }
+
+    inner class MyAccountsAdapter:BaseAdapter {
+        var listAccountsAdapter = ArrayList<Account>()
+        constructor(listAccountsAdapter:ArrayList<Account>):super() {
+            this.listAccountsAdapter = listAccountsAdapter
+
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+//          Adds each account to a new row in a list view
+            val rowView = layoutInflater.inflate(R.layout.account,null)
+            val account = listAccountsAdapter[position]
+            rowView.tvName.text = account.name
+            rowView.tvBalance.text = getString(R.string.balance_text, account.balance)
+            rowView.ivIcon.setImageResource(account.icon!!)
+            rowView.ivIcon.setBackgroundResource(account.circle!!)
+            return rowView
+        }
+
+        override fun getItem(position: Int): Any {
+            return listAccountsAdapter[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return listAccountsAdapter.size
+        }
     }
 }
