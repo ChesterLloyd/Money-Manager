f4ffab1feb57a394dee5e044dc10dd9e2f276225
Chester Lloyd <contact@chester-lloyd.co.uk>
2024-03-09 14:50:25 +0000

    Updated version and created a release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "dev.chester_lloyd.moneymanager"
         minSdk = 24
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.6.0"
+        versionCode = 11
+        versionName = "1.5.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
